fd8f6d13f3b441920add0feba0de272348c4bd42
Hanna Torrence <hanna.torrence@gmail.com>
2018-11-15 14:36:40 -0600

    expand test cases and logging

--- apparate/update_databricks_library.py
+++ apparate/update_databricks_library.py
@@ -5,7 +5,7 @@ The core of the program, update_databricks_library handles all the logic and
 """
 import json
 import re
-import os
+from os.path import basename
 
 import requests
 from configparser import NoOptionError
@@ -13,7 +13,6 @@ from simplejson.errors import JSONDecodeError
 
 from .configure import _load_config, CFG_FILE, PROFILE
 
-from hermiones_handbag import Timer
 
 class APIError(Exception):
     """
@@ -83,6 +82,7 @@ class FileNameMatch(object):
     def __init__(self, filename):
         match = re.match(FileNameMatch.file_pattern, filename)
         try:
+            self.filename = filename
             self.library_name = match.group(1)
             self.version = match.group(2)
             self.major_version = match.group(3)
@@ -97,9 +97,49 @@ class FileNameMatch(object):
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
+            self_attrs = {k: v for k, v in vars(self).items()}
+            self_attrs.pop('filename')
+            other_attrs = {k: v for k, v in vars(other).items()}
+            other_attrs.pop('filename')
+            return self_attrs == other_attrs
         else:
             return False
+
+    def replace_version(self, other, logger):
+        """
+        True if self can safely replace other
+
+        based on version numbers only - snapshot and branch tags are ignored
+        """
+
+        if other.library_name != self.library_name:
+            logger.debug(
+                'not replacable: {} != {} ()'
+                .format(other.library_name, self.library_name, other.filename)
+            )
+            return False
+        elif int(other.major_version) != int(self.major_version):
+            logger.debug(
+                'not replacable: {} != {} ({})'
+                .format(
+                    self.major_version,
+                    int(other.major_version),
+                    int(other.filename),
+                )
+            )
+            return False
+        elif float(other.minor_version) > float(self.minor_version):
+            logger.debug(
+                'not replacable: {} > {} ({})'
+                .format(
+                    other.minor_version,
+                    self.minor_version,
+                    other.filename,
+                )
+            )
+            return False
+        else:
+            return True
 
 
 def load_library(filename, match, folder, token, host):
@@ -139,14 +179,16 @@ def load_library(filename, match, folder, token, host):
         raise APIError(res)
 
 
-def get_job_list(match, library_mapping, token, host):
+def get_job_list(logger, match, library_mapping, token, host):
     """
     get a list of jobs using the major version of the given library
 
     Parameters
     ----------
+    logger: logging object
+        configured in cli_commands.py
     match: FilenameMatch object
-        match object with library_name, major_version, and suffix
+        match object with suffix
     library_mapping: dict
         first element of get_library_mapping output
     token: string
@@ -168,43 +210,42 @@ def get_job_list(match, library_mapping, token, host):
         if len(res.json()['jobs']) == 0:
             return []
         for job in res.json()['jobs']:
+            logger.debug('job: {}'.format(job['settings']['name']))
             if 'libraries' in job['settings'].keys():
                 for library in job['settings']['libraries']:
                     if match.suffix in library.keys():
                         try:  # if in prod_folder, mapping turns uri into name
-                            job_library_uri = (
-                                library[match.suffix].split('/')[-1]
-                            )
-                            job_library = library_mapping[job_library_uri]
+                            job_library_uri = basename(library[match.suffix])
+                            job_match = library_mapping[job_library_uri]
                         except KeyError:
+                            logger.debug(
+                                'not in library map: {}'
+                                .format(job_library_uri)
+                            )
                             pass
                         else:
-                            try:  # if we can parse a version, otherwise ignore
-                                job_library_name = (
-                                    '-'.join(job_library.split('-')[:-1])
-                                )
-                                job_major_version = (
-                                    job_library.split('-')[-1].split('.')[0]
-                                )
-                            except IndexError:
-                                pass
+                            if match.replace_version(job_match, logger):
+                                job_list.append({
+                                    'job_id': job['job_id'],
+                                    'job_name': job['settings']['name'],
+                                    'library_path': library[match.suffix],
+                                })
                             else:
-                                if (
-                                    match.library_name == job_library_name and
-                                    job_major_version == match.major_version
-                                ):
-                                    # append jobs we want to update later
-                                    job_list.append({
-                                        'job_id': job['job_id'],
-                                        'job_name': job['settings']['name'],
-                                        'library_path': library[match.suffix],
-                                    })
+                                logger.debug(
+                                    'not replacable: {}'
+                                    .format(job_match.filename)
+                                )
+                    else:
+                        logger.debug(
+                            'no matching suffix: looking for {}, found {}'
+                            .format(match.suffix, str(library.keys()))
+                        )
         return job_list
     else:
         raise APIError(res)
 
 
-def get_library_mapping(prod_folder, token, host):
+def get_library_mapping(logger, prod_folder, token, host):
     """
     returns a pair of library mappings, the first mapping library uri to a
      library name for all libraries in the production folder, and the second
@@ -213,6 +254,8 @@ def get_library_mapping(prod_folder, token, host):
 
     Parameters
     ----------
+    logger: logging object
+        configured in cli_commands.py
     prod_folder: string
         name of folder in Databricks UI containing production libraries
     token: string
@@ -227,12 +270,10 @@ def get_library_mapping(prod_folder, token, host):
     dictionary mapping library UI path to base name, major version,
         minor version, and id number
     """
-    t = Timer()
     res = requests.get(
         host + '/api/1.2/libraries/list',
         auth=('token', token),
     )
-    t.timecheck('got list of libraries')
     if res.status_code == 200:
         library_list = res.json()
         library_map = {}
@@ -250,39 +291,67 @@ def get_library_mapping(prod_folder, token, host):
                 library_info = status_res.json()
                 # only do any of this for libraries in the production folder
                 if library_info['folder'] != prod_folder:
-                    t.timecheck('skipping library: {}'.format(library))
-                    continue
-                library_map[library_info['files'][0]] = library_info['name']
-                try:
-                    print('', library_info['name'])
-                    name_split = library_info['name'].split('-')
-                    name_split = (
-                        ['-'.join(name_split[:-1])] + name_split[-1].split('.')
+                    logger.debug(
+                        'excluded folder: {} in {}, not prod folder ({})'
+                        .format(
+                            library_info['name'],
+                            library_info['folder'],
+                            prod_folder,
+                        )
                     )
-                    # map name to info
+                    continue
+                if library_info['libType'] == 'python-egg':
+                    full_name = library_info['name'] + '.egg'
+                elif library_info['libType'] == 'java-jar':
+                    full_name = library_info['name'] + '.jar'
+                else:
+                    logger.debug(
+                        'excluded library type: {} is of libType {}, '
+                        'not jar or egg'
+                        .format(
+                            library_info['name'],
+                            library_info['libType'],
+                        )
+                    )
+                    continue
+                try:
+                    name_match = FileNameMatch(full_name)
+                    # map uri to name match object
+                    library_map[library_info['files'][0]] = name_match
+                    # map name to name match object and id number
                     # we'll need the id number to clean up old libraries
                     id_nums[library_info['name']] = {
-                        'name': name_split[0],
-                        'major_version': name_split[1],
-                        'minor_version': '.'.join(name_split[2:]),
+                        'name_match': name_match,
                         'id_num': library_info['id'],
                     }
-                except IndexError:
+                except FileNameError:
+                    logger.debug(
+                        'FileNameError: {} file name is not parsable'
+                        .format(full_name)
+                    )
                     pass
             else:
                 raise APIError(status_res)
-            t.timecheck('processed library: {}'.format(library))
         return library_map, id_nums
     else:
         raise APIError(res)
 
 
-def update_job_libraries(job_list, match, new_library_path, token, host):
+def update_job_libraries(
+    logger,
+    job_list,
+    match,
+    new_library_path,
+    token,
+    host,
+):
     """
     update libraries on jobs using same major version
 
     Parameters
     ----------
+    logger: logging object
+        configured in cli_commands.py
     job_list: list of strings
         output of get_job_list
     match: FilenameMatch object
@@ -310,7 +379,6 @@ def update_job_libraries(job_list, match, new_library_path, token, host):
             settings = job_specs['settings']
             job_specs.pop('settings')
             new_libraries = []
-            print(settings['libraries'])
             for lib in settings['libraries']:
                 if (
                     match.suffix in lib.keys()
@@ -334,7 +402,8 @@ def update_job_libraries(job_list, match, new_library_path, token, host):
 
 
 def delete_old_versions(
-    match,
+    logger,
+    new_library_match,
     id_nums,
     token,
     prod_folder,
@@ -348,6 +417,8 @@ def delete_old_versions(
 
     Parameters
     ----------
+    logger: logging object
+        configured in cli_commands.py
     match: FilenameMatch object
         match object with library_name_, major_version, minor_version
     id_nums: dict
@@ -368,12 +439,8 @@ def delete_old_versions(
 
     old_versions = []
     for name, lib in id_nums.items():
-        if (
-            lib['name'] == match.library_name
-            and int(lib['major_version']) == int(match.major_version)
-            and float(lib['minor_version']) < float(match.minor_version)
-        ):
-            old_versions.append(name)
+        if new_library_match.replace_version(lib['name_match'], logger):
+            old_versions.append(lib['name_match'].filename)
             res = requests.post(
                 host + '/api/1.2/libraries/delete',
                 auth=('token', token),
@@ -384,7 +451,7 @@ def delete_old_versions(
     return old_versions
 
 
-def update_databricks(path, token, folder, update_jobs, cleanup):
+def update_databricks(logger, path, token, folder, update_jobs, cleanup):
     """
     upload library, update jobs using the same major version,
     and delete libraries with the same major and lower minor versions
@@ -392,6 +459,8 @@ def update_databricks(path, token, folder, update_jobs, cleanup):
 
     Parameters
     ----------
+    logger: logging object
+        configured in cli_commands.py
     path: string
         path with name of egg as output from setuptools
         (e.g. dist/new_library-1.0.0-py3.6.egg)
@@ -414,7 +483,6 @@ def update_databricks(path, token, folder, update_jobs, cleanup):
     if update_jobs is true, then updated jobs
     if update_jobs and cleanup are true, removed outdated libraries
     """
-    t = Timer()
 
     config = _load_config(CFG_FILE)
     try:
@@ -428,19 +496,17 @@ def update_databricks(path, token, folder, update_jobs, cleanup):
         raise ValueError('no prod_folder provided: please run '
                          '`apparate configure` to get set up')
 
-    # strip off prepended folders
-    filename = os.path.basename(path)
-    match = FileNameMatch(filename)
+    match = FileNameMatch(basename(path))
 
     try:
         load_library(path, match, folder, token, host)
-        print(
+        logger.info(
             'new library {}-{} loaded to Databricks'
             .format(match.library_name, match.version)
         )
     except APIError as err:
         if err.code == 'http 500' and 'already exists' in err.message:
-            print(
+            logger.info(
                 'this version ({}) already exists:'.format(match.version) +
                 'if a change has been made please update your version number'
             )
@@ -448,37 +514,51 @@ def update_databricks(path, token, folder, update_jobs, cleanup):
         else:
             raise err
 
-    t.timecheck('loaded')
-        
     if update_jobs and folder == prod_folder:
-        library_map, id_nums = get_library_mapping(prod_folder, token, host)
-        t.timecheck('library map')
+        library_map, id_nums = get_library_mapping(
+            logger,
+            prod_folder,
+            token,
+            host,
+        )
         library_uri = [
-            uri for uri, name in library_map.items()
-            if name == '{}-{}'.format(match.library_name, match.version)
+            uri for uri, tmp_match in library_map.items()
+            if (
+                match.library_name == tmp_match.library_name
+                and match.version == tmp_match.version
+            )
         ][0]
         library_path = 'dbfs:/FileStore/jars/' + library_uri
 
-        job_list = get_job_list(match, library_map, token, host)
-        t.timecheck('job list')
-        print(
+        job_list = get_job_list(logger, match, library_map, token, host)
+        logger.info(
             'current major version of library used by jobs: {}'
-            .format(', '.join([i['job_name'] for i in job_list]))
+            .format(',\n'.join([i['job_name'] for i in job_list]))
         )
 
         if len(job_list) != 0:
-            update_job_libraries(job_list, match, library_path, token, host)
-            print(
+            update_job_libraries(
+                logger,
+                job_list,
+                match,
+                library_path,
+                token,
+                host,
+            )
+            logger.info(
                 'updated jobs: {}'
-                .format(', '.join([i['job_name'] for i in job_list]))
+                .format(',\n'.join([i['job_name'] for i in job_list]))
             )
 
         if cleanup:
             old_versions = delete_old_versions(
+                logger,
                 match,
                 id_nums=id_nums,
                 token=token,
                 prod_folder=prod_folder,
                 host=host,
             )
-            print('removed old versions: {}'.format(', '.join(old_versions)))
+            logger.info(
+                'removed old versions: {}'.format(',\n'.join(old_versions))
+            )
